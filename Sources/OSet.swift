aa2cb31ca6efccae2a39994f24ce362f10ac91b0
KenjiTakahashi <wozniakk@gmail.com>
2017-10-15 22:51:22 +0200

    Docs

--- Sources/OSet.swift
+++ Sources/OSet.swift
@@ -17,22 +17,44 @@ public struct OSet<E: Hashable & Equatable>: SetAlgebra, MutableCollection, Rand
         self.s = Set()
     }
 
+    /// Checks if given item exists in the OSet.
+    ///
+    /// - Parameter item: An item to look for.
+    /// - Returns: `true` if item exists, `false` otherwise.
     public func contains(_ item: E) -> Bool {
         return self.s.contains(item)
     }
 
+    /// Adds items from given OSet to the OSet, returning a new copy.
+    /// If some items already exist in the original oset, they are kept.
+    ///
+    /// - Parameter other: An OSet to add to the current OSet.
+    /// - Returns: New OSet instance with items of both original and given OSet.
     public func union(_ other: OSet) -> OSet {
         return copy(self) { (s: inout OSet) in s.formUnion(other) }
     }
 
+    /// Gets items common among two OSets.
+    ///
+    /// - Parameter other: A second OSet to intersect with.
+    /// - Returns: New OSet instance with items contained in both original and given OSet.
     public func intersection(_ other: OSet) -> OSet {
         return copy(self) { (s: inout OSet) in s.formIntersection(other) }
     }
 
+    /// Gets items that are only in one OSet or the other, but not both.
+    ///
+    /// - Parameter other: A second OSet to differ with.
+    /// - Returns: New OSet instance with items contained either in original or in given OSet.
     public func symmetricDifference(_ other: OSet) -> OSet {
         return copy(self) { (s: inout OSet) in s.formSymmetricDifference(other) }
     }
 
+    /// Inserts new item at the end, if no such item is already present.
+    ///
+    /// - Parameter item: An item to insert.
+    /// - Returns: `(true, item)` if item got inserted, `(false, existingItem)` if and equal
+    ///   item already existed.
     @discardableResult
     public mutating func insert(_ item: E) -> (inserted: Bool, memberAfterInsert: E) {
         let (inserted, e) = self.s.insert(item)
@@ -42,6 +64,10 @@ public struct OSet<E: Hashable & Equatable>: SetAlgebra, MutableCollection, Rand
         return (inserted, e)
     }
 
+    /// Removes item.
+    ///
+    /// - Parameter item: An item to remove.
+    /// - Returns: Removed item, if it existed, `nil` otherwise.
     @discardableResult
     public mutating func remove(_ item: E) -> E? {
         let e = self.s.remove(item)
@@ -52,6 +78,10 @@ public struct OSet<E: Hashable & Equatable>: SetAlgebra, MutableCollection, Rand
         return e
     }
 
+    /// Updates an existing item, or inserts a new one at the end, if it didn't exist.
+    ///
+    /// - Parameter item: Element to update or insert.
+    /// - Returns: Old item, if it was updated, `nil` otherwise.
     @discardableResult
     public mutating func update(with item: E) -> E? {
         guard let existingItem = self.s.update(with: item) else {
@@ -61,12 +91,14 @@ public struct OSet<E: Hashable & Equatable>: SetAlgebra, MutableCollection, Rand
         return existingItem
     }
 
+    /// Performs `union` in place.
     public mutating func formUnion(_ other: OSet) {
         for item in other {
             self.insert(item)
         }
     }
 
+    /// Performs `intersection` in place.
     public mutating func formIntersection(_ other: OSet) {
         for item in self {
             if !other.contains(item) {
@@ -76,6 +108,7 @@ public struct OSet<E: Hashable & Equatable>: SetAlgebra, MutableCollection, Rand
         self.a = self.a.filter({ self.s.contains($0) })
     }
 
+    /// Performs `symmetricDifference` in place.
     public mutating func formSymmetricDifference(_ other: OSet) {
         for e in other {
             if self.contains(e) {
@@ -87,10 +120,28 @@ public struct OSet<E: Hashable & Equatable>: SetAlgebra, MutableCollection, Rand
         self.a = self.a.filter({ self.s.contains($0) })
     }
 
+    /// Checks whether two OSets are equal.
+    /// Note that this follows Array semantics, not Set ones, i.e
+    /// OSets with the same items, but at different positions are
+    /// reported as different.
+    ///
+    /// - Parameters:
+    ///   - lhs: First OSet to compare.
+    ///   - rhs: Second OSet to compare.
+    /// - Returns: `true` if OSets are equal, `false` otherwise.
     public static func == (lhs: OSet, rhs: OSet) -> Bool {
         return lhs.a == rhs.a
     }
 
+    /// Gets items that are only present in the first OSet and not the other.
+    ///
+    /// - Parameter other: An OSet to subtract with.
+    /// - Returns: New OSet instance with items only present in the original one.
+    public func subtracting(_ other: OSet) -> OSet {
+        return copy(self) { (s: inout OSet) in s.subtract(other) }
+    }
+
+    /// Performs `subtracting` in place.
     public mutating func subtract(_ other: OSet) {
         for e in other {
             self.s.remove(e)
@@ -98,35 +149,48 @@ public struct OSet<E: Hashable & Equatable>: SetAlgebra, MutableCollection, Rand
         self.a = self.a.filter({ self.s.contains($0) })
     }
 
-    public func subtracting(_ other: OSet) -> OSet {
-        return copy(self) { (s: inout OSet) in s.subtract(other) }
-    }
-
     // MARK: - SetAlgebra
     // MARK: Collection
 
     public typealias Iterator = IndexingIterator<[E]>
 
+    /// Returns an iterator view to the collection.
+    ///
+    /// - Returns: New iterator instance.
     public func makeIterator() -> Iterator {
         return self.a.makeIterator()
     }
 
+    /// The position of the first item in the OSet.
     public var startIndex: Int {
         return self.a.startIndex
     }
 
+    /// The position *after* the last item in the OSet.
     public var endIndex: Int {
         return self.a.endIndex
     }
 
+    /// Returns the position of item immediately after the given index.
+    ///
+    /// - Parameter after: An index valid for the current OSet instance.
+    /// - Returns: An index immediately after `after`.
     public func index(after: Int) -> Int {
         return self.a.index(after: after)
     }
 
+    /// Checks if OSet contains any items.
+    ///
+    /// - Returns: `true` if no items, `false`, otherwise.
     public var isEmpty: Bool {
         return self.a.isEmpty
     }
 
+    /// Swaps two items' positions (in place).
+    ///
+    /// - Parameters:
+    ///   - i: Index of the first item.
+    ///   - j: Index of the second item.
     public mutating func swapAt(_ i: Int, _ j: Int) {
         guard i != j else {
             return
@@ -139,6 +203,8 @@ public struct OSet<E: Hashable & Equatable>: SetAlgebra, MutableCollection, Rand
     // MARK: - Collection
     // MARK: Subscript
 
+    /// Gets or sets an item at the given index.
+    /// Enables the usual "`[]` notation" semantics.
     public subscript(index: Int) -> E {
         get {
             return self.a[index]
